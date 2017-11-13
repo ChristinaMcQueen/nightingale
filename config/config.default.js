72fd20d3bdd5f0290a37e12d995794404d60a2c5
qinming <272133340@qq.com>
2017-11-13 10:52:13 +0800

    init

--- config/config.default.js
+++ config/config.default.js
@@ -1,13 +1,11 @@
-'use strict';
-
 module.exports = appInfo => {
-  const config = exports = {};
+    const config = exports = {};
 
-  // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1510406440620_7743';
+    // use for cookie sign key, should change to your own and keep security
+    config.keys = appInfo.name + '_1510406440620_7743';
 
-  // add your config here
-  config.middleware = [];
+    // add your config here
+    config.middleware = [];
 
-  return config;
+    return config;
 };
